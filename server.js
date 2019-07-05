57563d79ed11c3163a4f3be9638791aa859aeaa5
WafflesAreFriends <johnfmunar@yahoo.com>
2019-07-04 20:04:07 -0700

    Updating database

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex')
 const db = knex({
 	client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-trapezoidal-23163',
     user : 'postgres',
     password : 'munarj',
     database : 'postgres'
